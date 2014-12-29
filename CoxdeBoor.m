b81f4297b6d3774f5b7c5d07d09d36af49f21493
Pieter Barendrecht <pieter@redpanda.nl>
2014-12-29 21:23:38 +0000

    Added LGPL headers.

--- CoxdeBoor.m
+++ CoxdeBoor.m
@@ -1,3 +1,16 @@
+% ----------------------------------------------------------------------
+
+% B-spline Lab is free software: you can redistribute it and/or modify it under the terms of the 
+% GNU Lesser General Public License as published by the Free Software Foundation, either version 3 
+% of the License, or (at your option) any later version.
+% 
+% B-spline Lab is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+% even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+% Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Lesser General Public License along with B-spline Lab. 
+% If not, see http://www.gnu.org/licenses/.
+
 % -----------------------------------------------------------------------
 
 function [Evals, Basis] = CoxdeBoor(Degree,Xi,From,To)
